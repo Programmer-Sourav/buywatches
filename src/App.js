d87124dbe9140db88e87032360df74f4775799b7
Programmer-Sourav <appsbucket333@gmail.com>
2023-05-16 00:51:10 +0530

    initial design of the project
    committing again after mock-bee integration

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
-import "./App.css";
-import logo from "./logo.png";
+import  FixedHeader from "./FixedHeader/FixedHeader"
+import ProductFilter from "./pages/ProductFilter"
+import ProductCard from "./Components/ProductCard";
+import ProductDescription from "./Components/ProductDescription";
+import ProductCardForWishlist from "./Components/ProductCardForWishlist"
+import MyCart from "./pages/MyCart"
+import LoginPage from "./pages/LoginPage";
+import { Routes, Route } from "react-router-dom"
+import MockMan from "mockman-js"
+
+
+import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+       {/* <FixedHeader/> */}
+       {/* <ProductFilter/> */}
+       {/* <ProductCard/> */}
+       {/* <ProductDescription/> */}
+       {/* <ProductCardForWishlist/> */}
+       {/* <MyCart/> */}
+       {/* <LoginPage/> */}
+       <Routes> 
+        <Route path="/" element={<h1>Hello Welcome</h1>}/>
+        <Route path="/mockman" element={<MockMan/>}/>
+       </Routes>
     </div>
   );
 }
