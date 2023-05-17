a4753c7f6cfc05b1af507220d218bdeb6f6b58d4
Programmer-Sourav <appsbucket333@gmail.com>
2023-05-17 22:02:20 +0530

    Update ProductsContext.js

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -5,16 +5,18 @@ export const ProductsContext = createContext();
 export function ProductsProvider({children}){
 
    const [ productsList, setProductsList ] = useState([]) /**State to hold data from api */
-
+   const [ loading, setLoading ] = useState(false)
 
    const getProducts = async() =>{
+    setLoading(true)
     try{
        const response = await fetch("/api/products",{
         method: 'GET'
     })
 
     const { products } = await response.json();
-    console.log(products)
+    //console.log(products)
+    setLoading(false)
     setProductsList(products)
     }
     catch(error){
@@ -26,7 +28,7 @@ export function ProductsProvider({children}){
    useEffect(()=>{getProducts()},[])
 
     return(
-        <ProductsContext.Provider value = {{productsList}}>{children}</ProductsContext.Provider>
+        <ProductsContext.Provider value = {{productsList, loading}}>{children}</ProductsContext.Provider>
     )
 
 }
