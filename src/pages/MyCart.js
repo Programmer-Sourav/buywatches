6838f4604ee22070bd5edb8ede0dcc1cb8891df9
Programmer-Sourav <appsbucket333@gmail.com>
2023-05-22 20:51:19 +0530

    cart
    ongoing work

--- src/pages/MyCart.js
+++ src/pages/MyCart.js
@@ -1,49 +1,63 @@
 import { useContext } from "react"
 import "../Stylesheets/mycart.css"
 import { CartContext } from "../contexts/CartContext"
+import { ACTION_TYPES_FOR_CART } from "../reducer/CartReducer"
 
 export default function MyCart(){
 
-    const { cart } = useContext(CartContext)
+    const { cart, cartDispatch } = useContext(CartContext)
     console.log("14566", cart.length)
+    //{_id, tag, brand, category, color, discounted_price, id, img, item, price, title}
+
+    const totalPrice = cart.reduce((acc, cur)=>acc+ cur.price,0)
+    console.log(1256, totalPrice)
+    const total_discounted_price = cart.reduce((acc, cur)=>acc+cur.discounted_price,0)
+    console.log(1257, total_discounted_price )
+    const total_discount = totalPrice-total_discounted_price;
+    console.log(1258, total_discount)
+    let initialQty = 1
+
+
     return(
         <div className="mycart-container"> 
             <h1> Shopping Cart</h1>
             <p> {cart.length} Item(s) is in your cart</p>
             <div className="mycart">
+            {  cart.map((cartItem)=>(
             <div className="mycart-style"> 
-            <img src="https://img1.gadgetsnow.com/gd/images/products/additional/large/G392624_View_2/accessories/smart-watches/apple-watch-ultra-gps-cellular-49-mm-retina-oled-display-green-alpine-loop-medium-smart-watch.jpg" alt="downloadedimage" className="image-cart-style"/>
-            <p className="item-name-cart"> <strong> Watch Ultra - by Apple </strong></p>
-            <span className="item-current-price-my-cart"><strong>₹79999</strong></span>
-            <button onClick={{}} style={{margin: "16px"}} className="button-small">-</button>
-            <span><strong>1</strong></span>
-            <button onClick={{}} style={{margin: "16px"}} className="button-small">+</button>
+            <img src={cartItem.img} alt="downloadedimage" className="image-cart-style"/>
+            <p className="item-name-cart"> <strong> {cartItem.item} </strong></p>
+            <span className="item-current-price-my-cart"><strong>{cartItem.discounted_price}</strong></span>
+            <button onClick={()=>{cartDispatch({type: ACTION_TYPES_FOR_CART.DECREMENT_QTY, payload: cartItem.id})}} style={{margin: "16px"}} className="button-small">-</button>
+            <span><strong>{initialQty}</strong></span>
+            <button onClick={()=>{cartDispatch({type: ACTION_TYPES_FOR_CART.INCREMENT_QTY, payload: cartItem.id })}} style={{margin: "16px"}} className="button-small">+</button>
             <div>
-            <button onClick={{}} className="button-bg-two">Remove from Cart</button>
+            <button onClick={()=>{cartDispatch({type: ACTION_TYPES_FOR_CART.REMOVE_FROM_CART, payload: cartItem })}} className="button-bg-two">Remove from Cart</button>
             <button onClick={{}} className="button-bg-three">Move to Wishlist</button>
             </div>
             </div>
+            ))}
             <div className="details-div">
     <p><strong>Price Details</strong></p>
     <hr/>
     <div className="cart-info">
     <span className="price-description"> Price- </span>
-    <span className="item-price-cart"> ₹89000 </span>
+    <span className="item-price-cart"> ₹{totalPrice}</span>
     <div>
     <span className="discount-description"> Discount- </span>
-    <span className="item-discount-cart">₹79000</span>
+    <span className="item-discount-cart">₹{total_discount}</span>
     </div>
     <span className="delivery"> Delivery Charges- </span>
-    <span> Rs. 40</span>
+    <span> ₹0</span>
     <div>
    
     <span className="total-amount"> Total Amount-</span>
-    <span>Rs. 4000</span>
+    <span>₹{total_discounted_price}</span>
     </div>
     </div>
     <hr/>
     <span>
-    <p className="saved-container"> Yay! You have saved ₹10000 in this order</p>
+    <p className="saved-container"> Yay! You have saved ₹{total_discount} in this order</p>
     </span>
     <button onClick={{}} className="checkout-button">Checkout</button>
     </div>
